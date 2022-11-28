886a8dc19b525d5fac9185c93d6c397dd0f037f8
Akash Chanara <akashchanara@gmail.com>
2022-11-27 18:40:41 -0800

    anime search

--- public/js/anime.js
+++ public/js/anime.js
@@ -1,58 +1,32 @@
-const url = "https://api.jikan.moe/v4";
+const base_url = "https://api.jikan.moe/v4";
 
 function searchAnime(event) {
   event.preventDefault();
-
   const form = new FormData(this);
   const query = form.get("search");
 
-  fetch(`${url}/search/anime?q=${query}&page=1`)
+  fetch(`${base_url}/anime?q=${query}&limit=5`)
     .then((res) => res.json())
-    .then(updateDom)
-    .catch((err) => console.log("error"));
-}
-
-function updateDom(data) {
-  const searchResults = document.getElementById("search-results");
-
-  const animeByCategories = data.results.reduce((acc, anime) => {
-    const { type } = anime;
-    if (acc[type] === undefined) acc[type] = [];
-    acc[type].push(anime);
-    return acc;
-  }, {});
-
-  searchResults.innerHTML = Object.keys(animeByCategories)
-    .map((key) => {
-      const animesHTML = animeByCategories[key]
-        .sort((a, b) => a.episodes - b.episodes)
-        .map((anime) => {
-          return `
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="${anime.image_url}">
-                        </div>
-                        <div class="card-content">
-                            <span class="card-title">${anime.title}</span>
-                            <p>${anime.synopsis}</p>
-                        </div>
-                        <div class="card-action">
-                            <a href="${anime.url}">Find out more</a>
-                        </div>
-                    </div>
-                `;
-        })
-        .join("");
-
-      return `
-                <section>
-                    <h3>${key.toUpperCase()}</h3>
-                    <div class="kemicofa-row">${animesHTML}</div>
-                </section>
-            `;
+    .then((data) => {
+      const list = data.data;
+      list.map((item) => {
+        const wer = item.title;
+        const imag = item.images.jpg.image_url;
+        console.log(wer);
+        const animme = `<img src="${imag}"><h2>${wer}</h2>`;
+        document.querySelector(".container").innerHTML += animme;
+      });
+      console.log(list);
     })
-    .join("");
+
+    .catch((err) => console.log(err.message));
 }
+// searchAnime();
+// function updateDom(data) {
+//   const searchResults = document.getElementById("search-results");
+
+//   return;
+// }
 
 function pageLoaded() {
   const form = document.getElementById("search_form");
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,6 +1,6 @@
 const sequelize = require("../config/connection");
 const { User, Post } = require("../models");
-
+// const anime = require("./anime-seed.json");
 const userData = require("./userData.json");
 const postData = require("./postData.json");
 
